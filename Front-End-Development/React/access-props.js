c12137b302204d22e1cdf5ee803e29eb46f172ce
Rogue Blue <rogueblueprint@protonmail.com>
2023-02-08 12:25:57 -0700

    Access props with this.props lesson added

---
+++ Front-End-Development/React/access-props.js
@@ -0,0 +1,27 @@
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+              <Welcome name="tom"/>
+          </div>
+      );
+    }
+};
+
+class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+
+    }
+    render() {
+      return (
+          <div>
+            <p>Hello, <strong>{this.props.name}</strong>!</p>
+          </div>
+      );
+    }
+};
