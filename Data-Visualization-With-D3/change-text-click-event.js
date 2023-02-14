c91a52244ae653bf7c8470a606c0b9eac683c982
Rogue Blue <rogueblueprint@protonmail.com>
2023-02-14 13:09:51 -0700

    Added lesson for changing an elements text with a click event

---
+++ Data-Visualization-With-D3/change-text-click-event.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+        document.getElementsByClassName('message')[0].textContent="Here is the message";
+    }
+});
